31f69bb67f610b258693c5f900d245ea827fd254
Oliver Richman <oliver.richman@outlook.com>
2023-04-06 14:43:50 +0100

    style: Adds ESLint, Prettier and fixes linting issues

--- src/utils.ts
+++ src/utils.ts
@@ -5,98 +5,99 @@ export const writeToTerminal = (textToWrite: string, clearPreviousText = false, 
 	const clearToStart = '\x1B[0G';
 	const newLine = addNewLine ? '\n' : '';
 	process.stdout.write(`${clearLine}${clearToStart}${textToWrite}${clearPreviousText ? '' : '\r'}${newLine}`);
-}
+};
 
 export const hideCursor = (): void => {
 	process.stdout.write('\u001b[?25l');
-}
+};
 
 export const showCursor = (): void => {
 	process.stdout.write('\u001b[?25h');
-}
+};
 
 export const getFirstKeyFromMap = (map: Map<any, any>) => {
-	return map.keys().next().value
-}
+	return map.keys().next().value;
+};
 
 export const getLastKeyFromMap = (map: Map<any, any>) => {
-	return Array.from(map)[map.size - 1][0]
-}
+	return Array.from(map)[map.size - 1][0];
+};
 
 export const createReadlineInterface = (): Interface => {
 	return createInterface({
 		input: process.stdin,
 		output: process.stdout,
 	});
-}
+};
 
 export const listenForKeyPress = (callback: (key: string, rli: Interface) => void) => {
-  emitKeypressEvents(process.stdin);
-  process.stdin.setRawMode(true);
-  process.stdin.resume();
-  const rli = createReadlineInterface();
+	emitKeypressEvents(process.stdin);
+	process.stdin.setRawMode(true);
+	process.stdin.resume();
+	const rli = createReadlineInterface();
 
-  process.stdin.on('keypress', (str, key) => {
-    if (key.ctrl && key.name === 'c') {
-      process.exit();
-    } else {
-      callback(key.name, rli);
-    }
-  });
+	process.stdin.on('keypress', (str, key) => {
+		if (key.ctrl && key.name === 'c') {
+			process.exit();
+		} else {
+			callback(key.name, rli);
+		}
+	});
 };
 
 export const joinWithConjunction = (array: (string | number | boolean)[], conjunction: string): string => {
-  if (!array.length) return "";
-  
-  const stringArray = array.map(item => item.toString());
-  const lastItem = stringArray.pop()!;
-  
-  if (!stringArray.length) return lastItem;
-  
-  const joinedString = stringArray.join(", ") + ` ${conjunction} ` + lastItem;
-  return joinedString;
-}
+	if (!array.length) return '';
 
-export const toCamelCase = (inputString: string): string => {
-  return inputString.replace(/[-_]+(.)?/g, (match, firstLetter) => {
-    if (firstLetter) {
-      return firstLetter.toUpperCase();
-    } else {
-      return '';
-    }
-  });
+	const stringArray = array.map((item) => item.toString());
+	const lastItem = stringArray.pop();
+
+	if (!lastItem) return stringArray.join(', ');
+
+	if (!stringArray.length) return lastItem;
+
+	const joinedString = stringArray.join(', ') + ` ${conjunction} ` + lastItem;
+	return joinedString;
 };
 
+export const toCamelCase = (inputString: string): string => {
+	return inputString.replace(/[-_]+(.)?/g, (match, firstLetter) => {
+		if (firstLetter) {
+			return firstLetter.toUpperCase();
+		} else {
+			return '';
+		}
+	});
+};
 
 export const ansiCodes = {
-  reset: 0,
-  bold: 1,
-  dim: 2,
-  italic: 3,
-  underline: 4,
-  black: 30,
-  blackBg: 40,
-  red: 31,
-  redBg: 41,
-  green: 32,
-  greenBg: 42,
-  yellow: 33,
-  yellowBg: 43,
-  blue: 34,
-  blueBg: 44,
-  magenta: 35,
-  magentaBg: 45,
-  cyan: 36,
-  cyanBg: 46,
-  white:37,
-  whiteBg: 47,
+	reset: 0,
+	bold: 1,
+	dim: 2,
+	italic: 3,
+	underline: 4,
+	black: 30,
+	blackBg: 40,
+	red: 31,
+	redBg: 41,
+	green: 32,
+	greenBg: 42,
+	yellow: 33,
+	yellowBg: 43,
+	blue: 34,
+	blueBg: 44,
+	magenta: 35,
+	magentaBg: 45,
+	cyan: 36,
+	cyanBg: 46,
+	white: 37,
+	whiteBg: 47,
 };
 
 export const removeDuplicateResetCodes = (message: string): string => {
-  const regex = /(\\x1b\[0m){2,}/g;
-  return message.replace(regex, '\x1b[0m');
-}
+	const regex = /(\\x1b\[0m){2,}/g;
+	return message.replace(regex, '\x1b[0m');
+};
 
 export const getAnsiCode = (style: string): string => {
-  return `\x1b[${ansiCodes[style]}m`;
-}
+	return `\x1b[${ansiCodes[style]}m`;
+};
